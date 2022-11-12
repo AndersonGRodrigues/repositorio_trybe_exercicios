4b894519ca58516d24dc69b70da898159aa31130
AndersonGRodrigues <andersonrodrigues0854@gmail.com>
2022-11-12 20:13:57 -0300

    sucesso na requisisão da funcionalidade de busca informação na url

--- front_end/casaDeCambio/src/main.js
+++ front_end/casaDeCambio/src/main.js
@@ -1,14 +1,16 @@
 const button = document.querySelector('button');
 const textCoin = document.querySelector('input');
+const guardaDados = document.querySelector('#dados')
 
 button.addEventListener('click', funcionalidade)
 
-function funcionalidade() {
-    const url = 'https://api.exchangerate.host/latest?base=USD'
-    fetch(url)
-        .then((resposta) => resposta.json())
-        .then((data) => {
-            console.log(data.rates)
-        })
+const text = textCoin.value
+
+function funcionalidade(text) {
+  const obj = '';
+  const url = `https://api.exchangerate.host/latest?base=${text}`
+  return fetch(url)
+    .then(resposta => resposta.json())
+    .then(data => data.rates)
+    .then(date => console.log(Object.entries(date)))
 }
-funcionalidade()
