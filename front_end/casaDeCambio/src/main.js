ab7a3a381f6ff1ce59eb574af512b0d858c1d6fd
AndersonGRodrigues <andersonrodrigues0854@gmail.com>
2022-11-13 10:38:21 -0300

    quebrei a função funcionalidade e a dividir em tratamentoApi e createLi pra melhor entendimento

--- front_end/casaDeCambio/src/main.js
+++ front_end/casaDeCambio/src/main.js
@@ -7,16 +7,29 @@ button.addEventListener('click', funcionalidade)
 const url = (text) => {
   return `https://api.exchangerate.host/latest?base=${text}`
 }
-
-function funcionalidade() {
-  return fetch(url(textCoin.value))
+// const removeLi = () => {
+//   const li = document.querySelector('li')
+//   guardaDados.removeChild(li)
+// }
+// funcionalidade é chamada por button ao event de click, criar li e joga resultado da API dentro
+function tratamentoApi() {
+  // removeLi()
+    return fetch(url(textCoin.value))
     .then(resposta => resposta.json())
     .then(data => data.rates)
     .then(date => Object.entries(date))
-    .then(dateArray => dateArray.map( data => {
-      const li = document.createElement('li')
-      li.innerHTML = `${data}`;
-      guardaDados.appendChild(li)
-    }))
 }
 
+function createLi(data) {
+  data.map((data) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${data[0]} ${data[1]}`;
+    guardaDados.appendChild(li);
+  })
+
+}
+
+function funcionalidade() {
+  tratamentoApi()
+    .then((data) => createLi(data))
+}
