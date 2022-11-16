456f7e434ae6ac1bf16fc5e1d073834e69dd70ee
AndersonGRodrigues <andersonrodrigues0854@gmail.com>
2022-11-16 09:55:42 -0300

    comentarios inseridos para melhor intendimento

--- front_end/casaDeCambio/src/main.js
+++ front_end/casaDeCambio/src/main.js
@@ -1,35 +1,33 @@
 const button = document.querySelector('button');
 const textCoin = document.querySelector('input');
-const guardaDados = document.querySelector('#dados')
+const guardaDados = document.querySelector('#dados');
+const clear = document.querySelectorAll('#clear')
 
-button.addEventListener('click', funcionalidade)
+button.addEventListener('click', funcionalidade);
 
 const url = (text) => {
   return `https://api.exchangerate.host/latest?base=${text}`
 }
-// const removeLi = () => {
-//   const li = document.querySelector('li')
-//   guardaDados.removeChild(li)
-// }
-// funcionalidade é chamada por button ao event de click, criar li e joga resultado da API dentro
+//busca a Api e faz seu tratamento de string para objeto de objeto para Array
 function tratamentoApi() {
-  // removeLi()
     return fetch(url(textCoin.value))
     .then(resposta => resposta.json())
     .then(data => data.rates)
     .then(date => Object.entries(date))
 }
-
+// crear as li dentro de guarda dados'
 function createLi(data) {
-  data.map((data) => {
+  data.map(data => {
     const li = document.createElement('li');
-    li.innerHTML = `${data[0]} ${data[1]}`;
+    li.innerHTML = `${data[0]} - ${data[1]}`;
     guardaDados.appendChild(li);
   })
-
 }
-
+// funcionalidade é chamada por button ao event de click.
 function funcionalidade() {
   tratamentoApi()
-    .then((data) => createLi(data))
+    .then(data => {
+      createLi(data)
+    })
+  console.log(guardaDados.childElementCount)
 }
